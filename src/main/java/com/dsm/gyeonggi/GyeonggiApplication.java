3a419acc52d7f1bb2232df1ff2a8d2e0ab235bcf
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-09-13 15:12:06 +0900

    [UPDATE] Start Project

---
+++ src/main/java/com/dsm/gyeonggi/GyeonggiApplication.java
@@ -0,0 +1,13 @@
+package com.dsm.gyeonggi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyeonggiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyeonggiApplication.class, args);
+	}
+
+}
