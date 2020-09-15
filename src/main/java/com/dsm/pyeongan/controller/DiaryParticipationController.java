e5f06098f69f6a22c1fdabb6c423fff527b55e6a
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-09-15 19:45:36 +0900

    [UPDATE] Swagger Work

--- src/main/java/com/dsm/pyeongan/controller/DiaryParticipationController.java
+++ src/main/java/com/dsm/pyeongan/controller/DiaryParticipationController.java
@@ -1,22 +1,36 @@
 package com.dsm.pyeongan.controller;
 
 import com.dsm.pyeongan.domains.service.DiaryParticipationService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/diary")
+@Api(value = "Attendance Controller")
 public class DiaryParticipationController {
 
-    @Autowired
     private DiaryParticipationService diaryParticipationService;
 
+    @Autowired
+    public DiaryParticipationController(DiaryParticipationService diaryParticipationService) {
+        this.diaryParticipationService = diaryParticipationService;
+    }
+
+    @ApiOperation(value = "일기장 참여", notes = "participation_personnel_list 테이블에 code, User 매칭")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "정상적인 응답"),
+            @ApiResponse(code = 404, message = "인증 실패 또는 code 중복"),
+            @ApiResponse(code = 500, message = "500")
+    })
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "header", name = "token", dataType = "string", required = true, value = "Access Token")
+    })
     @PostMapping("/participation-record")
-    public void participate(HttpServletRequest request, String code) {
+    public void participate(@ApiParam(value = "030816", required = true) @RequestParam("code") String code,
+                            HttpServletRequest request) {
         String accessToken = request.getHeader("accessToken");
         // accessToken 인증 해야함 :: MSA
 
