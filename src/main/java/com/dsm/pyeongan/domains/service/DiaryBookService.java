2963eacb5a0344b41246bd513fe17770fd07f13d
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-22 17:08:41 +0900

    <DiaryBookService> kotlin -> java

---
+++ src/main/java/com/dsm/pyeongan/domains/service/DiaryBookService.java
@@ -0,0 +1,23 @@
+package com.dsm.pyeongan.domains.service;
+
+import com.google.gson.GsonBuilder;
+import okhttp3.OkHttpClient;
+import org.springframework.stereotype.Service;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+@Service
+public class DiaryBookService {
+    public void connectingRoom(String userId, String code) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://gyeongsang:8893")
+                .client(new OkHttpClient().newBuilder().build())
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().setLenient().create()))
+                .build();
+
+        DiaryBookRequestConnectionService service = retrofit.create(DiaryBookRequestConnectionService.class);
+        service.addRequest(userId, code);
+    }
+}
