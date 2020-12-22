b55ee3a0b260f5c429b8bbe05e96071761921673
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-22 16:53:51 +0900

    <CodeRequestForm> create CodeRequestForm [kotlin -> java]

---
+++ src/main/java/com/dsm/pyeongan/utils/form/CodeRequestForm.java
@@ -0,0 +1,18 @@
+package com.dsm.pyeongan.utils.form;
+
+public class CodeRequestForm {
+    private String code;
+
+    public CodeRequestForm() {}
+    public CodeRequestForm(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
