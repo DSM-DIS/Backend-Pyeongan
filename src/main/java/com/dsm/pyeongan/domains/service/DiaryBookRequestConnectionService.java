69901ad19fe721f7cd53b7700799282fba33afce
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-31 08:43:28 +0900

    <DiaryBookRequestConnectionService> Call<Void> -> void

--- src/main/java/com/dsm/pyeongan/domains/service/DiaryBookRequestConnectionService.java
+++ src/main/java/com/dsm/pyeongan/domains/service/DiaryBookRequestConnectionService.java
@@ -1,10 +1,9 @@
 package com.dsm.pyeongan.domains.service;
 
-import retrofit2.Call;
 import retrofit2.http.*;
 
 public interface DiaryBookRequestConnectionService {
     @Headers(value = {"accept: application/json", "content-type: application/json"})
     @POST("/repositories/diary-book")
-    Call<Void> addRequest(@Header("userId") String userId, @Body String code);
+    void addRequest(@Header("userId") String userId, @Body String code);
 }
