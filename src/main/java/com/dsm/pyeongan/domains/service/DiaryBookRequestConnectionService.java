6dfea80c03e46fce6f0503f606418d1559f39624
gimhaesanai <jinjin0816@naver.com>
2021-01-01 18:38:40 +0900

    d

--- src/main/java/com/dsm/pyeongan/domains/service/DiaryBookRequestConnectionService.java
+++ src/main/java/com/dsm/pyeongan/domains/service/DiaryBookRequestConnectionService.java
@@ -6,5 +6,5 @@ import retrofit2.http.*;
 public interface DiaryBookRequestConnectionService {
     @Headers(value = {"accept: application/json", "content-type: application/json"})
     @POST
-    Call<String> addRequest(@Url String url, @Header("userId") String userId, @Body String code);
+    Call<String> addRequest(@Url String url, @Header("userId") String userId, @Body String body);
 }
